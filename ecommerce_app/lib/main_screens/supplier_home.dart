b5629cd310d34e258be951e5fdda038e8589d118
Saarim Khan <saarimkhan515@gmail.com>
2022-11-17 19:13:01 +0530

    Profile Screen and Suppilier Screen is done

---
+++ ecommerce_app/lib/main_screens/supplier_home.dart
@@ -0,0 +1,64 @@
+import 'package:ecommerce_app/main_screens/cart.dart';
+import 'package:ecommerce_app/main_screens/category.dart';
+import 'package:ecommerce_app/main_screens/home.dart';
+import 'package:ecommerce_app/main_screens/profile.dart';
+import 'package:ecommerce_app/main_screens/stores.dart';
+import 'package:flutter/material.dart';
+
+class SupplierHomeScreen extends StatefulWidget {
+  const SupplierHomeScreen({super.key});
+
+  @override
+  State<SupplierHomeScreen> createState() => _SupplierHomeScreenState();
+}
+
+class _SupplierHomeScreenState extends State<SupplierHomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _tabs =const [
+    HomeScreen(),
+    CategoryScreen(),
+    StoresScreen(),
+    Center(child: Text('Dashboard'),),
+    Center(child: Text('Upload'),)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Stores',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.upload),
+            label: 'Upload',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index; // telling that i have tapped on this icon
+          });
+        },
+      ),
+    );
+  }
+}
